5c2af7aabb1e18d6f243ed2c06601d51bcc3e008
Connor <connor_ford1@baylor.edu>
2023-02-13 14:15:41 -0600

    after class 2_13

--- InsectClass.py
+++ InsectClass.py
@@ -2,9 +2,10 @@ import random
 
 
 class Insect:
-    def __init__(self):
-        self.wings = 2
-        self.legs = 4
+    def __init__(self, n, w, l):
+        self.name = n
+        self.wings = w
+        self.legs = l
         self.lengthOfFlight = 0
 
     def setFlight(self):
@@ -12,3 +13,6 @@ class Insect:
 
     def getFlight(self):
         return self.lengthOfFlight
+
+    def getName(self):
+        return self.name
--- InsectProgram.py
+++ InsectProgram.py
@@ -2,14 +2,14 @@ import InsectClass as i
 
 
 def main():
-    house_fly = i.Insect()
+    house_fly = i.Insect("House Fly", 2, 4)
     house_fly.setFlight()
-    mosquito = i.Insect()
+    mosquito = i.Insect("Mosquito", 2, 4)
     mosquito.setFlight()
 
-    print("House fly flight length: " + str(house_fly.getFlight()))
+    print(house_fly.getName() + " flight length: " + str(house_fly.getFlight()))
     print()
-    print("Mosquito flight length: " + str(mosquito.getFlight()))
+    print(mosquito.getName() + " flight length: " + str(mosquito.getFlight()))
 
 
 main()
